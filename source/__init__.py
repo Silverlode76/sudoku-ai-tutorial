546f8c2caa4ffc8377704eef270469a827a11e9a
Silverlode76 <schmoll.oliver@web.de>
2025-12-29 12:40:13 +0100

    Colab Adjust

--- source/__init__.py
+++ source/__init__.py
@@ -1,3 +1,4 @@
-from .core import probs_from_logits, pretty_grid_from_probs, print_losses
-from .losses import sudoku_losses
+from .core import probs_from_logits, pretty_grid_from_probs, print_losses, check_grid
+from .losses import sudoku_losses, entropy_mean
 from .optimize import optimize_sudoku, loss_dict
+
